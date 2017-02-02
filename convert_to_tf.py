a3dbdb67719c68aabcbbff8f95bd5efe5c954149
James Cuénod <j3frea@gmail.com>
2017-02-01 22:39:43 -0600

    Major fix for detecting offsets

--- convert_to_tf.py
+++ convert_to_tf.py
@@ -56,16 +56,19 @@ for i in reversed(range(len(sdbh_contents) - 2)):
 		del sdbh_contents[i+1]
 		sdbh_contents[i]["hebrew"] = "ל" + sdbh_contents[i+1]["hebrew"]
 
+
 # Get words from SDBH
-sdbh_words = []
-sdbh_words_references = []
-for match in sdbh_contents:
-	if not match["id"].startswith("Gen "):
-		continue
-	if match["hebrew"] == "ס" or match["hebrew"] == "ף":
-		continue
-	sdbh_words.append(match["hebrew"])
-	sdbh_words_references.append(match["id"])
+def get_sdbh(book_abbreviation):
+	sdbh_words = []
+	sdbh_references = []
+	for match in sdbh_contents:
+		if not match["id"].startswith(book_abbreviation):
+			continue
+		if match["hebrew"] == "ס" or match["hebrew"] == "ף":
+			continue
+		sdbh_words.append(match["hebrew"])
+		sdbh_references.append(match["id"])
+	return sdbh_words, sdbh_references
 
 
 TF = Fabric(locations='/home/jcuenod/Programming/text-fabric-data', modules='hebrew/etcbc4c')
@@ -73,77 +76,107 @@ api = TF.load('g_cons_utf8')
 api.makeAvailableIn(globals())
 
 
-verse_node = T.nodeFromSection(('Genesis', 1, 1))
-book_node = L.u(verse_node, otype='book')[0]
-tf_words = []
-tf_references = []
-for w in L.d(book_node, otype='word'):
-	if F.g_cons_utf8.v(w) != "":
-		tf_words.append(F.g_cons_utf8.v(w))
-		tf_references.append(str(T.sectionFromNode(w)))
+def get_tf(book_abbreviation):
+	verse_node = T.nodeFromSection((book_abbreviation, 1, 1), lang='sblspaceless')
+	book_node = L.u(verse_node, otype='book')[0]
+	tf_words = []
+	tf_references = []
+	for w in L.d(book_node, otype='word'):
+		if F.g_cons_utf8.v(w) != "":
+			tf_words.append(F.g_cons_utf8.v(w))
+			tf_references.append(str(T.sectionFromNode(w)))
+	return tf_words, tf_references
 
-offset_counter = 0
-def increment_offset_counter(offset_counter):
-	offset_counter += 1
-	# if offset_counter % 2500 == 0:
-	# 	print("offsets: ", offset_counter)
-	return offset_counter
+def replace_finals(word):
+	return word.replace("ך", "כ") \
+		.replace("ם", "מ") \
+		.replace("ן", "נ") \
+		.replace("ף", "פ") \
+		.replace("ץ", "צ")
 
-final_chars = set('ךםןףץ')
+def _norm(w):
+	return replace_finals(unicodedata.normalize("NFKD", w)).replace(" ", "")
 
-counter = 0
-print("sdbh units: ", len(sdbh_words))
-print("  tf units: ", len(tf_words))
-offset_sdbh = 0
-offset_tf = 0
-for i in range(len(min(sdbh_words, tf_words))):
-	if unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf]):
-		# if offset_tf > 1 or offset_sdbh:
-		# 	print("offset help!")
-		continue
+def normalised_compare(w1, w2):
+	return _norm(w1) == _norm(w2)
 
-	# just assume that if it's just a final character, it's been wrongly converted
-	if sdbh_words[i - offset_sdbh] in final_chars:
-		continue
+do_print = True
+# for book_abbreviation in sdbh_books:
+for book_abbreviation in ["1Chr"]:
+	tf_words, tf_references = get_tf(book_abbreviation)
+	sdbh_words, sdbh_references = get_sdbh(book_abbreviation)
+	offset_counter = 0
+	def increment_offset_counter(offset_counter):
+		offset_counter += 1
+		# if offset_counter % 2500 == 0:
+		# 	print("offsets: ", offset_counter)
+		return offset_counter
 
-	#detect offsets
-	if unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh - 1]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf]):
-		offset_sdbh += 1
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
-	elif unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf - 1]):
-		offset_tf += 1
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
-	elif unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh - 2]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf]):
-		offset_sdbh += 2
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
-	elif unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf - 2]):
-		offset_tf += 2
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
-	elif unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh - 3]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf]):
-		offset_sdbh += 3
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
-	elif unicodedata.normalize("NFKD", sdbh_words[i - offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i - offset_tf - 3]):
-		offset_tf += 3
-		offset_counter = increment_offset_counter(offset_counter)
-		continue
+	counter = 0
+	if do_print:
+		print("sdbh units: ", len(sdbh_words))
+		print("  tf units: ", len(tf_words))
+	offset_sdbh = 0
+	offset_tf = 0
+	for i in range(len(min(sdbh_words, tf_words))):
+		if normalised_compare(sdbh_words[i + offset_sdbh], tf_words[i + offset_tf]):
+			# if offset_tf > 1 or offset_sdbh:
+			# 	print("offset help!")
+			continue
+		elif len(sdbh_words[i + offset_sdbh]) > len(tf_words[i + offset_tf]):
+			print("adasdfasdf")
+			to_test = tf_words[i + offset_tf] + tf_words[i + offset_tf + 1]
+			temp_offset = offset_tf + 1
+			while len(_norm(to_test)) < len(_norm(sdbh_words[i + offset_sdbh])):
+				temp_offset += 1
+				to_test += tf_words[i + temp_offset]
+			if normalised_compare(sdbh_words[i + offset_sdbh], to_test):
+				offset_tf = temp_offset
+				print("  tf fix:", sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "==", to_test, " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
+				# data from sdbh for one word goes to two...
+				continue
+		elif len(tf_words[i + offset_tf]) > len(sdbh_words[i + offset_sdbh]):
+			to_test = sdbh_words[i + offset_sdbh] + sdbh_words[i + offset_sdbh + 1]
+			temp_offset = offset_sdbh + 1
+			while len(_norm(to_test)) < len(_norm(tf_words[i + offset_tf])):
+				temp_offset += 1
+				to_test += sdbh_words[i + temp_offset]
+			if normalised_compare(tf_words[i + offset_tf], to_test):
+				offset_sdbh = temp_offset
+				print("sdbh fix:", sdbh_references[i + offset_sdbh], ":SD: ", to_test, "=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
+				# data from sdbh for one word goes to two...
+				continue
 
-	print(sdbh_words_references[i - offset_sdbh], ":SD: ", sdbh_words[i - offset_sdbh], "=!=", tf_words[i - offset_tf], " :TF:", tf_references[i - offset_tf])
+		# just assume that if it's just a final character, it's been wrongly converted
+		# if sdbh_words[i + offset_sdbh] in final_chars:
+		# 	continue
 
-	counter += 1
-	if counter > 100:
-		print("TOO MANY ISSUES - EXITING")
-		exit()
+		#detect offsets
+		# if unicodedata.normalize("NFKD", sdbh_words[i + offset_sdbh + 1]) == unicodedata.normalize("NFKD", tf_words[i + offset_tf]):
+		# 	print(sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " BUT ", sdbh_words[i + offset_sdbh + 1])
+		# 	offset_sdbh += 1
+		# 	offset_counter = increment_offset_counter(offset_counter)
+		# 	continue
+		# elif unicodedata.normalize("NFKD", sdbh_words[i + offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i + offset_tf + 1]):
+		# 	print(sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " BUT ", tf_words[i + offset_tf + 1])
+		# 	offset_tf += 1
+		# 	offset_counter = increment_offset_counter(offset_counter)
+		# 	continue
 
-print(offset_sdbh)
-print(offset_tf)
+		if do_print:
+			print(sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
 
-if len(sdbh_words) != len(tf_words):
-	print("\nlast 5 words:")
-	print("sdbh_words: ", " ".join(sdbh_words[len(sdbh_words) - 5:]), sdbh_words_references[-1])
-	print("  tf_words: ", " ".join(tf_words[len(tf_words) - 5:]), tf_references[-1])
-	print("WARNING: lists of unequal length, not all data has been compared")
+		counter += 1
+		if counter > 100:
+			print("TOO MANY ISSUES - EXITING")
+			exit()
+
+	if do_print:
+		print(offset_sdbh)
+		print(offset_tf)
+
+		if len(sdbh_words) != len(tf_words):
+			print("\nlast 5 words:")
+			print("sdbh_words: ", " ".join(sdbh_words[len(sdbh_words) - 5:]), sdbh_references[-1])
+			print("  tf_words: ", " ".join(tf_words[len(tf_words) - 5:]), tf_references[-1])
+			print("WARNING: lists of unequal length, not all data has been compared")
