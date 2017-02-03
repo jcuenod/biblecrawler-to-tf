0346fef17c0e6cd7737b69ec8b3e19db9c858bfb
James Cuénod <j3frea@gmail.com>
2017-02-02 23:32:50 -0600

    First attempt at an actual sdbh.tf

--- convert_to_tf.py
+++ convert_to_tf.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime
+
 from tf.fabric import Fabric
 import csv
 import unicodedata
@@ -13,8 +15,49 @@ qere = 0
 rafe = 0
 line = ""
 sdbh_contents = []
-sdbh_books = ['Hab', 'Prov', 'Esth', 'Eccl', 'Ps', 'Jonah', 'Mal', 'Neh', 'Hos', 'Exod', 'Joel', 'Obad', '1Chr', 'Judg', '1Sam', 'Ezra', 'Num', 'Ruth', 'Deut', 'Dan', 'Jer', 'Lev', '2Chr', '2Kgs', 'Isa', 'Zech', '2Sam', 'Lam', 'Gen', 'Mic', 'Song', 'Nah', 'Hag', 'Job', 'Zeph', 'Ezek', 'Josh', '1Kgs', 'Amos']
+sdbh_books = [
+	"Gen",
+	"Exod",
+	"Lev",
+	"Num",
+	"Deut",
+	"Josh",
+	"Judg",
+	"1Sam",
+	"2Sam",
+	"1Kgs",
+	"2Kgs",
+	"Isa",
+	"Jer",
+	"Ezek",
+	"Hos",
+	"Joel",
+	"Amos",
+	"Obad",
+	"Jonah",
+	"Mic",
+	"Nah",
+	"Hab",
+	"Zeph",
+	"Hag",
+	"Zech",
+	"Mal",
+	"Ps",
+	"Job",
+	"Prov",
+	"Ruth",
+	"Song",
+	"Eccl",
+	"Lam",
+	"Esth",
+	"Dan",
+	"Ezra",
+	"Neh",
+	"1Chr",
+	"2Chr",
+]
 
+print("Reading csv ...")
 with open('sdbh.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
@@ -23,12 +66,9 @@ with open('sdbh.csv') as csvfile:
 			if solution:
 				sdbh_contents = sdbh_contents + solution
 			continue
-
-
 		sdbh_counter += 1
 		to_decode = re.sub(r'[\d\/]', '', row['HebrewText'])
 		to_decode = re.sub(r'~', ' ', to_decode)
-
 		if re.search(r'\*\*', to_decode):
 			qere+=1
 			continue
@@ -38,37 +78,31 @@ with open('sdbh.csv') as csvfile:
 		if re.search(r',', to_decode):
 			to_decode = re.sub(r'\,', '', to_decode)
 			rafe += 1
-
 		if to_decode == "_":
 			continue
-
 		sdbh_contents.append({
 			"id": row["RecordId"],
 			"betacode": row["HebrewText"],
-			"hebrew": decode(to_decode)
+			"hebrew": decode(to_decode),
+			"domain": row["LexDomain"],
+			"sdbh": row["SDBH"]
 		})
-
-
-#clean up some funny stuff
-l_suffix = ["מה", "כן"]
-for i in reversed(range(len(sdbh_contents) - 2)):
-	if sdbh_contents[i]["hebrew"] == "ל" and sdbh_contents[i+1]["hebrew"] in l_suffix:
-		del sdbh_contents[i+1]
-		sdbh_contents[i]["hebrew"] = "ל" + sdbh_contents[i+1]["hebrew"]
-
+print("Completed csv prep")
 
 # Get words from SDBH
 def get_sdbh(book_abbreviation):
 	sdbh_words = []
 	sdbh_references = []
-	for match in sdbh_contents:
+	sdbh_ids = []
+	for i, match in enumerate(sdbh_contents):
 		if not match["id"].startswith(book_abbreviation):
 			continue
-		if match["hebrew"] == "ס" or match["hebrew"] == "ף":
+		if match["hebrew"] == "ס" or match["hebrew"] == "ף" or match["hebrew"] == "ן":
 			continue
 		sdbh_words.append(match["hebrew"])
 		sdbh_references.append(match["id"])
-	return sdbh_words, sdbh_references
+		sdbh_ids.append(i)
+	return sdbh_words, sdbh_references, sdbh_ids
 
 
 TF = Fabric(locations='/home/jcuenod/Programming/text-fabric-data', modules='hebrew/etcbc4c')
@@ -81,11 +115,13 @@ def get_tf(book_abbreviation):
 	book_node = L.u(verse_node, otype='book')[0]
 	tf_words = []
 	tf_references = []
+	tf_ids = []
 	for w in L.d(book_node, otype='word'):
 		if F.g_cons_utf8.v(w) != "":
 			tf_words.append(F.g_cons_utf8.v(w))
 			tf_references.append(str(T.sectionFromNode(w)))
-	return tf_words, tf_references
+			tf_ids.append(w)
+	return tf_words, tf_references, tf_ids
 
 def replace_finals(word):
 	return word.replace("ך", "כ") \
@@ -100,11 +136,15 @@ def _norm(w):
 def normalised_compare(w1, w2):
 	return _norm(w1) == _norm(w2)
 
+
+
+node_data = []
+domains_to_ignore = ["Negators", "Identifiers"]
 do_print = True
-# for book_abbreviation in sdbh_books:
-for book_abbreviation in ["1Chr"]:
-	tf_words, tf_references = get_tf(book_abbreviation)
-	sdbh_words, sdbh_references = get_sdbh(book_abbreviation)
+# for book_abbreviation in ["Ps"]:
+for book_abbreviation in sdbh_books:
+	tf_words, tf_references, tf_ids = get_tf(book_abbreviation)
+	sdbh_words, sdbh_references, sdbh_ids = get_sdbh(book_abbreviation)
 	offset_counter = 0
 	def increment_offset_counter(offset_counter):
 		offset_counter += 1
@@ -114,57 +154,101 @@ for book_abbreviation in ["1Chr"]:
 
 	counter = 0
 	if do_print:
-		print("sdbh units: ", len(sdbh_words))
-		print("  tf units: ", len(tf_words))
+		print(book_abbreviation, "SDBH units: ", len(sdbh_words), " TF units: ", len(tf_words))
 	offset_sdbh = 0
 	offset_tf = 0
-	for i in range(len(min(sdbh_words, tf_words))):
+	i = -1
+	while i + offset_tf + 1 < len(tf_words) and i + offset_sdbh + 1 < len(sdbh_words):
+		i += 1
+		# for i in range(len(min(sdbh_words, tf_words))):
 		if normalised_compare(sdbh_words[i + offset_sdbh], tf_words[i + offset_tf]):
-			# if offset_tf > 1 or offset_sdbh:
-			# 	print("offset help!")
+			# if write_to_file:
+				# write to file
+			node_data.append((_norm(sdbh_words[i+offset_sdbh]), sdbh_contents[sdbh_ids[i+offset_sdbh]]["domain"], sdbh_references[i+offset_sdbh]))
+			continue
+		elif sdbh_words[i + offset_sdbh] == "ישׂשכר" and tf_words[i + offset_tf] == "ישׂשׂכר":
+			# variant spelling of Issachar
+			node_data.append((_norm(sdbh_words[i+offset_sdbh]), sdbh_contents[sdbh_ids[i+offset_sdbh]]["domain"], sdbh_references[i+offset_sdbh]))
 			continue
 		elif len(sdbh_words[i + offset_sdbh]) > len(tf_words[i + offset_tf]):
-			print("adasdfasdf")
+			# data from sdbh for one word goes to two...
 			to_test = tf_words[i + offset_tf] + tf_words[i + offset_tf + 1]
 			temp_offset = offset_tf + 1
 			while len(_norm(to_test)) < len(_norm(sdbh_words[i + offset_sdbh])):
 				temp_offset += 1
 				to_test += tf_words[i + temp_offset]
 			if normalised_compare(sdbh_words[i + offset_sdbh], to_test):
+				for j in range(temp_offset - offset_tf):
+					node_data.append((_norm(sdbh_words[i+offset_sdbh]), sdbh_contents[sdbh_ids[i+offset_sdbh]]["domain"], sdbh_references[i+offset_sdbh]))
 				offset_tf = temp_offset
-				print("  tf fix:", sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "==", to_test, " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
-				# data from sdbh for one word goes to two...
+				# if do_print:
+				# 	print("  tf fix:", sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "==", to_test, " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
 				continue
 		elif len(tf_words[i + offset_tf]) > len(sdbh_words[i + offset_sdbh]):
+			# data from multiple sdbh goes to one tf word...
 			to_test = sdbh_words[i + offset_sdbh] + sdbh_words[i + offset_sdbh + 1]
 			temp_offset = offset_sdbh + 1
 			while len(_norm(to_test)) < len(_norm(tf_words[i + offset_tf])):
 				temp_offset += 1
 				to_test += sdbh_words[i + temp_offset]
 			if normalised_compare(tf_words[i + offset_tf], to_test):
+				possibles = []
+				last_resort = []
+				prefer_me = []
+				for j in range(temp_offset - offset_sdbh + 1):
+					if sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"] == "":
+						# ignore empty lexical domains
+						continue
+					if sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"] in domains_to_ignore:
+						# ignore certain domains
+						last_resort.append((_norm(sdbh_words[i+offset_sdbh+j]), sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"], sdbh_references[i+offset_sdbh+j]))
+						continue
+					if len(_norm(sdbh_words[i+offset_sdbh+j])) == 1:
+						# ignore certain domains
+						last_resort.append((_norm(sdbh_words[i+offset_sdbh+j]), sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"], sdbh_references[i+offset_sdbh+j]))
+						continue
+					if sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"] in map(lambda x: x[1], possibles):
+						# if we already have this category, ignore it a second time
+						#although maybe we should use the second one instead?
+						continue
+					if sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"] == "Names of Locations":
+						# ignore names of locations -- maybe we will just end up using this value
+						prefer_me.append((_norm(sdbh_words[i+offset_sdbh+j]), sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"], sdbh_references[i+offset_sdbh+j]))
+						continue
+					possibles.append((_norm(sdbh_words[i+offset_sdbh+j]), sdbh_contents[sdbh_ids[i+offset_sdbh+j]]["domain"], sdbh_references[i+offset_sdbh+j]))
+
+				if len(possibles) > 1:
+					print("POSSIBLES:", possibles)
+				elif len(possibles) == 0:
+					if len(prefer_me) > 0:
+						if len(prefer_me) == 1:
+							node_data.append(prefer_me[0])
+						else:
+							print("PREFER: ", prefer_me)
+					elif len(last_resort) > 0:
+						if len(last_resort) == 1:
+							node_data.append(last_resort[0])
+						else:
+							print("LAST RESORT:", last_resort)
+					else:
+						# print("LAST RESORT/PREFER: not enough options...")
+						node_data.append(("","",""))
+				else:
+					if len(prefer_me) > 1:
+						if len(prefer_me) == 1:
+							node_data.append(prefer_me[0])
+						else:
+							print("PREFER: ", prefer_me)
+					else:
+						node_data.append(possibles[0])
+				# if do_print:
+				# 	print("sdbh fix:", sdbh_references[i + offset_sdbh], ":SD: ", to_test, "=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
+
 				offset_sdbh = temp_offset
-				print("sdbh fix:", sdbh_references[i + offset_sdbh], ":SD: ", to_test, "=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
-				# data from sdbh for one word goes to two...
 				continue
 
-		# just assume that if it's just a final character, it's been wrongly converted
-		# if sdbh_words[i + offset_sdbh] in final_chars:
-		# 	continue
-
-		#detect offsets
-		# if unicodedata.normalize("NFKD", sdbh_words[i + offset_sdbh + 1]) == unicodedata.normalize("NFKD", tf_words[i + offset_tf]):
-		# 	print(sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " BUT ", sdbh_words[i + offset_sdbh + 1])
-		# 	offset_sdbh += 1
-		# 	offset_counter = increment_offset_counter(offset_counter)
-		# 	continue
-		# elif unicodedata.normalize("NFKD", sdbh_words[i + offset_sdbh]) == unicodedata.normalize("NFKD", tf_words[i + offset_tf + 1]):
-		# 	print(sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " BUT ", tf_words[i + offset_tf + 1])
-		# 	offset_tf += 1
-		# 	offset_counter = increment_offset_counter(offset_counter)
-		# 	continue
-
-		if do_print:
-			print(sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
+		# if do_print:
+		# 	print(sdbh_references[i + offset_sdbh], ":SD: ", sdbh_words[i + offset_sdbh], "=!=", tf_words[i + offset_tf], " :TF:", tf_references[i + offset_tf], "OFFSETS (tf, sdbh):", offset_tf, offset_sdbh)
 
 		counter += 1
 		if counter > 100:
@@ -172,11 +256,24 @@ for book_abbreviation in ["1Chr"]:
 			exit()
 
 	if do_print:
-		print(offset_sdbh)
-		print(offset_tf)
-
-		if len(sdbh_words) != len(tf_words):
+		if len(sdbh_words) - offset_sdbh != len(tf_words) - offset_tf:
 			print("\nlast 5 words:")
 			print("sdbh_words: ", " ".join(sdbh_words[len(sdbh_words) - 5:]), sdbh_references[-1])
 			print("  tf_words: ", " ".join(tf_words[len(tf_words) - 5:]), tf_references[-1])
 			print("WARNING: lists of unequal length, not all data has been compared")
+		else:
+			print("success")
+
+
+filename = "sdbh.tf"
+fileheader = '''@node
+@valueType=str
+@writtenBy=James Cuénod & SDBH
+@dateWritten={0}
+
+'''.format(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))
+print("writing file:", filename)
+
+with open(filename, mode='wt', encoding='utf-8') as out:
+	out.write(fileheader)
+	out.write('\n'.join(map(lambda x: x[1], node_data)))
--- rules.py
+++ rules.py
@@ -1,39 +1,130 @@
 rules = []
+# rules.append({
+# 	"affected_records": ["Jer 29:23,18.1", "Jer 29:23,18.2"],
+# 	"lexical_data": [{
+# 		"id": "Jer 29:23,18.1",
+# 		"betacode": "*HW.",
+# 		"hebrew": "ה",
+# 		"SDBH":"HW.)_1a",
+# 		"lexical_domain":"Participants"
+# 	},{
+# 		"id": "Jer 29:23,18.2",
+# 		"betacode": "*YOD\"(A",
+# 		"hebrew": "וידע",
+# 		"SDBH":"YD(_1a",
+# 		"lexical_domain":"Know"
+# 	}]
+# })
 rules.append({
-	"affected_records": ["Exod 6:6,1.1", "Exod 6:6,1.2"],
+	"affected_records": ["Judg 1:36,3.2", "Judg 1:36,4.1"],
 	"lexical_data": [{
-		"id": "Exod 6:6,1.1",
-		"betacode": "L.FMF70H",
-		"hebrew": "לכן",
-		"SDBH":"",
-		"lexical_domain":""
+		"id": "Judg 1:36,3.2",
+		"betacode": "M.A(:AL\"73H (AQ:RAB.I92YM",
+		"hebrew": "מעלה עקרבים",
+		"sdbh":"",
+		"domain":"Names of Locations"
 	}]
 })
 rules.append({
-	"affected_records": ["Song 6:12,6.1"],
+	"affected_records": ["2Kgs 9:27,17.2", "2Kgs 9:27,18.1"],
 	"lexical_data": [{
-		"id": "Song 6:12,6.1",
-		"betacode": "(AM.IY-",
-		"hebrew": "עמי",
-		"SDBH":"",
-		"lexical_domain":""
-	},{
-		"id": "Song 6:12,6.2",
-		"betacode": "NFDI75YB00",
-		"hebrew": "נדיב",
-		"SDBH":"",
-		"lexical_domain":""
-	},]
+		"id": "2Kgs 9:27,17.2",
+		"betacode": "M.A(:AL\"73H (AQ:RAB.I92YM",
+		"hebrew": "מעלה עקרבים",
+		"sdbh":"",
+		"domain":"Names of Locations"
+	}]
 })
 rules.append({
-	"affected_records": ["Song 7:5,9.1"],
+	"affected_records": ["2Kgs 6:25,16.1", "2Kgs 6:25,16.2"],
 	"lexical_data": [{
-		"id": "Song 7:5,9.1",
-		"betacode": "B.AT-RAB.I80YM",
-		"hebrew": "בת רבים",
-		"SDBH":"B.AT~RAB.IYM_1a",
-		"lexical_domain":"Names of Locations"
+		"id": "2Kgs 6:25,16.1",
+		"betacode": "X:AR""Y YOWNIYM",
+		"hebrew": "חרייונים",
+		"sdbh":"X:ARF)IYM_1a",
+		"domain":"Body Products"
 	}]
+})
+
+rules.append({
+	"affected_records": ["Num 34:4,5.2", "Num 34:4,6.1"],
+	"lexical_data": [{
+		"id": "Num 34:4,5.2",
+		"betacode": "M.A(:AL\"73H (AQ:RAB.I92YM",
+		"hebrew": "מעלה עקרבים",
+		"sdbh":"",
+		"domain":"Names of Locations"
+	}]
+})
+rules.append({
+	"affected_records": ["Josh 15:3,4.2", "Josh 15:3,5.1"],
+	"lexical_data": [{
+		"id": "Josh 15:3,4.2",
+		"betacode": "M.A(:AL\"73H (AQ:RAB.I92YM",
+		"hebrew": "מעלה עקרבים",
+		"sdbh":"",
+		"domain":"Names of Locations"
+	}]
+})
+
+rules.append({
+	"affected_records": ["1Chr 4:12,10.1", "1Chr 4:12,11.1"],
+	"lexical_data": [{
+		"id": "1Chr 4:12,10.1",
+		"betacode": "M.A(:AL\"73H (AQ:RAB.I92YM",
+		"hebrew": "עיר נחשׁ",
+		"sdbh":"",
+		"domain":"Names of Locations"
+	}]
+})
+
+rules.append({
+	"affected_records": ["Ps 22:2,3.1", "Ps 22:2,3.2"],
+	"lexical_data": [{
+		"id": "Ps 22:2,3.1",
+		"betacode": "MF74H",
+		"hebrew": "למה",
+		"sdbh":")\"L.EH_1a",
+		"domain":"Identifiers"
+	}]
+})
+
+rules.append({
+	"affected_records": ["Job 19:29,11.1", "Job 19:29,11.2"],
+	"lexical_data": [{
+		"id": "Job 19:29,11.1",
+		"betacode": "$AD.IYN",
+		"hebrew": "שׁדין",
+		"sdbh":"D.IYN",
+		"domain":"Laws"
+	}]
+})
+
+# Do to these as to Jeremiah (no - I'm just going to apply the sdbh categories kind of loosely)
+#"Eccl 6:10,14.1",*$E,$E_1a,"Relations of Description",*$E
+#"Eccl 6:10,14.2",*HAT:QIYP,TQP_1b,Strong,*HAT:QIYP
+#"Ps 102:4,6.1",K.:,K._1a,"Relations of Description",K.:
+#"Ps 102:4,6.2","MOW-Q""71D",,,"MOW-Q""71D"
+#"Neh 2:13,16.1",*HA,H_1a,Identifiers,*HA
+#"Neh 2:13,16.2",*M:PORWFCIYM,PRC_1b,Impact,*M:PORWFCIYM
+#"2Kgs 7:15,14.1",*B.:,,,*B.:
+#"2Kgs 7:15,14.2","*H""XFP:Z/FM",XPZ_1a,Attitude,"*H""XFP:Z/FM"
+
+# Remove the bizarro things in here...
+rules.append({
+	"affected_records": [ \
+		"2Kgs 19:31,10.1", \
+		"2Kgs 19:37,9.1", \
+		"2Sam 8:3,13.1", \
+		"2Sam 16:23,9.1", \
+		"2Sam 18:20,19.1", \
+		"Jer 31:38,3.1", \
+		"Jer 50:29,13.1", \
+		"Judg 20:13,15.1", \
+		"Ruth 3:5,6.1", \
+		"Ruth 3:17,9.1" \
+	],
+	"lexical_data": []
 })
 
 
